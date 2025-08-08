14e79d28363b78d19d70b13e8a237a5c2893a718
Dallin Weller <weller.dallin@gmail.com>
2025-08-08 09:02:49 -0600

    Update Negative.js

--- test/specs/SearchBar.js/Negative.js
+++ test/specs/SearchBar.js/Negative.js
@@ -1,5 +1,4 @@
-//npx wdio run wdio.conf.js --spec ./test/specs/SearchBar.js/Negative.js 
-//npx wdio run wdio.conf.js --spec test/specs/SearchBar.js/*
+
 import SearchBar from '../../pageobjects/searchBar.js';
 
 describe('[Target] Search Bar - Single Character Security Input Handling', () => {
